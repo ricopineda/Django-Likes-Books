bba1bf0b6f475e1df70ffca4c708f08a47fa3561
ricopineda <ricopineda@me.com>
2017-08-16 18:23:40 -0700

    completed Project that had a ton of migrations errors

---
+++ apps/book_app/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from django.db import models
+# Create your models here.
+class User(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	email = models.CharField(max_length=255)
+
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+
+
+class Book(models.Model):
+	name = models.CharField(max_length=255)
+
+	desc = models.TextField(default=None)
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+	# uploaded_by_id = models.IntegerField()
+	likes = models.ManyToManyField(User, related_name = "liked_books")
+	uploader = models.ForeignKey(User, blank= True, null=True, related_name = "upload_books")
+
+
